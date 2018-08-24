7768c65056035a4a029fcb036a14d0f468860910
DanielaMun <41449250+DanielaMun@users.noreply.github.com>
2018-08-24 15:55:48 -0500

    App.xam.css

--- Propina/Propina/MainPage.xaml.cs
+++ Propina/Propina/MainPage.xaml.cs
@@ -1,17 +1,81 @@
 ﻿using System;
+
 using System.Collections.Generic;
+
 using System.Linq;
+
 using System.Text;
+
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
 
-namespace Propina
+
+
+namespace AppPropinas
+
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
+
+    public partial class MainPage : ContentPage
+
+    {
+
+        public MainPage()
+
+        {
+
+            InitializeComponent();
+
+            btncalcu.Clicked += btnClicked;
+
+        }
+
+
+
+        private void btnClicked(Object sender, EventArgs e)
+
+        {
+
+            /* Convertir  a numero el total, propina, numero de personas*/
+
+            double total = double.Parse(toti.Text);
+
+            double propina = double.Parse(propi.Text);
+
+            double numeropersona = double.Parse(numper.Text);
+
+            /* Se genera las operaciones previas */
+
+
+
+            double respropina = (total * propina) / 100;
+
+            double restotal = respropina + total;
+
+            double rtapropinapersona = respropina / numeropersona;
+
+            double restotpersona = restotal / numeropersona;
+
+
+
+            txtpropina.Detail = propina.ToString("C");
+
+            txttotal.Detail = restotal.ToString("C");
+
+            txtpropersona.Detail = rtapropinapersona.ToString("C");
+
+            txttotalpersona.Detail = restotpersona.ToString("C");
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+
 }
